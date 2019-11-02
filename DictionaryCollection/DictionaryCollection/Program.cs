0ec186ca4798b1ef0ccecedd8cb1752e11c96b6a
BaseDorp <44095612+BaseDorp@users.noreply.github.com>
2019-11-02 01:53:14 -0500

    Algo_SH_TextSortLinq
    Program that meets assignment requirements

--- DictionaryCollection/DictionaryCollection/Program.cs
+++ DictionaryCollection/DictionaryCollection/Program.cs
@@ -13,25 +13,67 @@ namespace DictionaryCollection
         {
             // Directory that the text file is going to be read in from
             string textFile = Directory.GetCurrentDirectory() + @"\words.txt";
-            // Turn to a list if I have to
             string[] words = File.ReadAllLines(textFile);
 
-            //// Sort list from Z-A
-            //for (int i = words.Length-1; i > -1; i--)
-            //{
-            //    Console.WriteLine(words[i]);
-            //}
+            Console.WriteLine("1. Sort list from Z-A\n2. Return all words starting with 'Z'\n3. Return all words that start with 'He'\n4. Return all words that the second letter is an 'e'\n");
+            string input;
+            input = Console.ReadLine();
+            Console.Clear();
+            switch (input)
+            {
+                case "1":
+                    ReverseSort();
+                    break;
+                case "2":
+                    StringStartsWith("Z");
+                    break;
+                case "3":
+                    StringStartsWith("He");
+                    break;
+                case "4":
+                    SecondLetter();
+                    break;
+                default:
+                    // Default path in case user types in not a valid number
+                    ReverseSort();
+                    break;
+            }
 
-            // Return all words starting with 'Z'
-            var queryResults =
-                from n in words
-                where n.StartsWith("Z")
-                select n;
-            Console.WriteLine(queryResults);
+            // Sort list from Z-A
+            void ReverseSort()
+            {
+                for (int i = words.Length - 1; i > -1; i--)
+                {
+                    Console.WriteLine(words[i]);
+                }
+            }
 
-            // Return all words that start with 'He'
+            // Return all words that start with a letter
+            // Part 2 and 3 use the same code so I just use the same method
+            void StringStartsWith(string _letter)
+            {
+                //Code sourced from http://www.java2s.com/Tutorial/CSharp/0450__LINQ/LINQquerytogetstringsstartingwiths.htm
+                var queryResults =
+                    from n in words
+                    where n.StartsWith(_letter)
+                    select n;
+
+                foreach (var item in queryResults)
+                {
+                    Console.WriteLine(item);
+                }
+            }
 
             // Return all words that the second letter is an 'e'
+            void SecondLetter()
+            {
+                // Source code from https://stackoverflow.com/questions/43413889/how-to-select-a-name-that-contains-the-second-letter-is-s-using-linq
+                var result = words.Where(s => s.Length > 1 && s[1] == 'e');
+                foreach (var item in result)
+                {
+                    Console.WriteLine(item);
+                }
+            }
 
             Console.WriteLine("Press Any Key to Continue ...");
             Console.ReadKey();
